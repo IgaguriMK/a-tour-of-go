fe6b64150e27bbd2627c5c01e70d693daac358f4
igagurimk2 <igagurimk@gmail.com>
2017-12-06 14:38:34 +0900

    'Exercise: reader'を完了

---
+++ exercise-reader.go
@@ -0,0 +1,17 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (_ MyReader) Read(b []byte) (int, error) {
+	n := len(b)
+	for i := 0; i < n; i++ {
+		b[i] = 'A'
+	}
+	return n, nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
